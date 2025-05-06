d0639ab5f3747c4ba6f0d5ae4ac48235e5110aea
Sophie <sophiesokol123@gmail.com>
2025-05-06 13:51:40 +0100

    Products now take info from database and not sample data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,12 @@
-import sampleData from '@/db/sample-data';
 import ProductList from '@/components/shared/product/product-list';
+import { getLatestProducts } from '@/lib/actions/product.actions';
 import React from 'react';
 
 const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
   return (
     <div>
-      <ProductList data={sampleData.products} title='Newest Arrivals' limit={4} />
+      <ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
     </div>
   );
 }
