b56cdb944aec2757b9f32fbedac73a9e7063d7e8
Sophie <sophiesokol123@gmail.com>
2025-05-06 12:30:18 +0100

    Finish basic UI with sample data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,11 @@
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/shared/product/product-list';
 import React from 'react';
 
 const HomePage = async () => {
   return (
     <div>
+      <ProductList data={sampleData.products} title='Newest Arrivals' limit={4} />
     </div>
   );
 }
