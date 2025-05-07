acc57506299382dd5238967fca47c091e9e350e2
Sophie <sophiesokol123@gmail.com>
2025-05-07 13:33:21 +0100

    Create product type

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ProductCard from './product-card';
+import { Product } from '@/types';
 
 interface ProductListTypes {
-  data: any,
+  data: Product[],
   title?: string;
   limit?: number;
 }
@@ -15,7 +16,7 @@ const ProductList: React.FC<ProductListTypes> = ({ data, title, limit }) => {
       <h2 className='h2-bold mb-4'>{title}</h2>
       {data.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {limitedData.map((product: any) => (<ProductCard key={product.slug} product={product} />))}
+          {limitedData.map((product: Product) => (<ProductCard key={product.slug} product={product} />))}
         </div>
       ) : (
         <div>
