43df93fad0274b4a5f203b7d5bf16c0d725cefea
Sophie <sophiesokol123@gmail.com>
2025-05-12 15:39:44 +0100

    Add error messages to the sign up form

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
+import { formatError } from "../utils";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(prevState: unknown, formData: FormData) {
@@ -70,6 +71,6 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
       throw error;
     }
 
-    return { success: false, message: 'Failed to create new user.' }
+    return { success: false, message: formatError(error) }
   }
 }
--- lib/validators.ts
+++ lib/validators.ts
@@ -9,13 +9,13 @@ const currency = z
 
 // Schema for inserting products
 export const insertProductsSchema = z.object({
-  name: z.string().min(3, 'Name must be at least 3 characters.'),
-  slug: z.string().min(3, 'Slug must be at least 3 characters.'),
-  category: z.string().min(3, 'Category must be at least 3 characters.'),
-  brand: z.string().min(3, 'Brand must be at least 3 characters.'),
-  description: z.string().min(3, 'Description must be at least 3 characters.'),
+  name: z.string().min(3, 'Name must be at least 3 characters'),
+  slug: z.string().min(3, 'Slug must be at least 3 characters'),
+  category: z.string().min(3, 'Category must be at least 3 characters'),
+  brand: z.string().min(3, 'Brand must be at least 3 characters'),
+  description: z.string().min(3, 'Description must be at least 3 characters'),
   stock: z.coerce.number(),
-  images: z.array(z.string()).min(1, 'Product must have at least one image.'),
+  images: z.array(z.string()).min(1, 'Product must have at least one image'),
   isFeatured: z.boolean(),
   banner: z.string().nullable(),
   price: currency,
@@ -24,15 +24,15 @@ export const insertProductsSchema = z.object({
 // Schema for signing users in
 export const signInFormSchema = z.object({
   email: z.string().email('Invalid email address'),
-  password: z.string().min(6, 'Password must be at least 6 characters.')
+  password: z.string().min(6, 'Password must be at least 6 characters')
 });
 
 // Schema for signing up a user
 export const signUpFormSchema = z.object({
   name: z.string().min(3, 'Name must be at least 3 characters'),
   email: z.string().email('Invalid email address'),
-  password: z.string().min(6, 'Password must be at least 6 characters.'),
-  confirmPassword: z.string().min(6, 'Confirm password must be at least 6 characters.')
+  password: z.string().min(6, 'Password must be at least 6 characters'),
+  confirmPassword: z.string().min(6, 'Confirm password must be at least 6 characters')
 }).refine((data) => data.password === data.confirmPassword, {
   message: 'Passwords do not match',
   path: ['confirmPassword'],
