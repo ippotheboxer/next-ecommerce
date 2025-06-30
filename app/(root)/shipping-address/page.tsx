b2e351c8056b9af52a6c6b102ed7530dba1ee687
Sophie <sophiesokol123@gmail.com>
2025-06-30 16:06:42 +0100

    Create shipping address form

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -4,6 +4,7 @@ import { getUserById } from "@/lib/actions/user.actions";
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { ShippingAddress } from "@/types";
+import ShippingAddressForm from "./shipping-address-form";
 
 export const metadata: Metadata = {
   title: 'Shipping Address'
@@ -23,7 +24,9 @@ const ShippingAddressPage = async () => {
   const user = await getUserById(userId);
 
   return (
-    <>Address</>
+    <>
+      <ShippingAddressForm address={user.address as ShippingAddress} />
+    </>
   )
 }
 
