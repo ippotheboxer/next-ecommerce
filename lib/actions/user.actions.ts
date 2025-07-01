23f56218e5186928b891706dee3fe098beeff8b4
Sophie <sophiesokol123@gmail.com>
2025-07-01 12:52:40 +0100

    Create update user payment method action and zod schema

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,11 @@
 'use server';
 
-import { shippingAddressSchema, signInFormSchema, signUpFormSchema } from "../validators";
+import {
+  shippingAddressSchema,
+  signInFormSchema,
+  signUpFormSchema,
+  paymentMethodSchema
+} from "../validators";
 import { auth, signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { hashSync } from "bcrypt-ts-edge";
@@ -108,6 +113,32 @@ export async function updateUserAddress(data: ShippingAddress) {
     });
 
     return { success: true, message: 'User updated successfully' }
+  } catch (error) {
+    return { success: false, message: formatError(error) }
+  }
+}
+
+// Update user's payment method
+export async function updateUserPaymentMethod(data: z.infer<typeof paymentMethodSchema>) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id }
+    });
+
+    if (!currentUser) throw new Error('User not found');
+
+    const paymentMethod = paymentMethodSchema.parse(data);
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type }
+    });
+
+    return {
+      success: true,
+      message: 'User updated successfully'
+    }
   } catch (error) {
     return { success: false, message: formatError(error) }
   }
