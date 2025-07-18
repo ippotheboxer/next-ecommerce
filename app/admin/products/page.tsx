076e0e06ffc2b2c566fa061588635b2c29dbb1a6
Sophie <sophiesokol123@gmail.com>
2025-07-18 13:44:32 +0100

    Get products for admin page

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,27 @@
+import { getAllProducts } from "@/lib/actions/product.actions";
+import Link from "next/link";
+import { formatCurrency, formatId } from "@/lib/utils";
+
+const AdminProductsPage = async (props: {
+  searchParams: Promise<{
+    page: string;
+    query: string;
+    category: string;
+  }>
+}) => {
+  const searchParams = await props.searchParams;
+
+  const page = Number(searchParams.page) || 1;
+  const searchText = searchParams.query || '';
+  const category = searchParams.category || '';
+
+  return (
+    <div className="space-y-2">
+      <div className="flex-between">
+        <h1 className="h2-bold">Products</h1>
+      </div>
+    </div>
+  );
+}
+
+export default AdminProductsPage;
